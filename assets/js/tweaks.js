c210f2c5464c010fd95c12947f3b659720be36e6
Chris Krycho <chris@krycho.com>
2015-07-22 08:22:39 -0500

    Update documentation

--- assets/js/tweaks.js
+++
@@ -1,9 +0,0 @@
-(function ($) {
-  // Find all the relevant paragraphs: the first in an article, the first after
-  // an hrule, and the first after any header.
-  let $firstP = $('.content p').first(),
-      $hrPs = $('hr + p'),
-      $h1Ps = $('h1 + p'),
-      allPs = $().add($firstP).add($hrPs).add($h1Ps);
-
-})(jQuery);
