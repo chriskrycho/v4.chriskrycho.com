35c86e57f68edfada02b78e61d6466348f0033e2
Chris Krycho <chris@chriskrycho.com>
2016-03-31 21:47:12 -0400

    Add spacing for quotation marks in Spacewell.
    Resolves #82.

--- 2014_theme/scripts/lib.js
+++ 2014_theme/scripts/lib.js
@@ -2,7 +2,7 @@ import spacewell from './spacewell';
 // import smcp from 'scmp';
 
 const articles = document.getElementsByTagName('article');
-const options = {'emDashes': true, 'enDashes': true};
+const options = { emDashes: true, enDashes: true, quotes: true };
 for (const a in articles) {
   if (articles.hasOwnProperty(a)) { spacewell(articles[a], options); }
 }
