d40d6df12ff8cfacac0065c763f1ad721584811a
Chris Krycho <hello@chriskrycho.com>
2018-10-07 13:20:03 -0600

    Fix `lib.js`. Stupid `HTMLCollection` not being an array.

--- theme/scripts/lib.js
+++ theme/scripts/lib.js
@@ -1,6 +1,7 @@
 import spacewell from "./spacewell";
 // import smcp from 'scmp';
 
-document
-   .getElementsByTagName("article")
-   .forEach(spacewell({ emDashes: true, enDashes: true, quotes: true }));
+const process = spacewell({ emDashes: true, enDashes: true, quotes: true });
+for (const article of document.getElementsByTagName("article")) {
+   process(article);
+}
